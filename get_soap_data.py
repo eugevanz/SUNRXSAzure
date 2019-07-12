f65d9cc1757a0a214b56bb58d27f3f3b31c58a5f
Eugene Van Zyl <eugevanz@gmail.com>
2019-07-12 12:26:01 +0200

    Office Fabric

--- get_soap_data.py
+++ get_soap_data.py
@@ -1,21 +1,22 @@
+
 import zeep
 from zeep import xsd
 
 wsdl = 'http://nlwremrvq01.mtn.co.za:8080/arsys/WSDL/public/nlaremrvq01.mtn.co.za/MWE_MTN_PMC_GET_INCIDENT_DATA?wsdl'
 client = zeep.Client(wsdl=wsdl)
 
-# auth_info = client.get_type('ns0:AuthenticationInfo')
-# login = auth_info(userName='174877', password='Geenas_3040')
 def auth():
     username = xsd.Element('userName', xsd.String())
     password = xsd.Element('password', xsd.String())
     return xsd.ComplexType([username, password])
 
 header = xsd.Element('AuthenticationInfo', auth())
-header_value = header(userName='174877', password='Geenas_3040')
+header_value = header(userName='174877', password='mtn123')
 
 try:
-    response = client.service.Get_Incident_Data(Qualification='?', startRecord='?', maxLimit=100, _soapheaders=[header_value])
+    response = client.service.Get_Incident_Data(Qualification="'Incident Number' = \"?\"", startRecord="", maxLimit="", _soapheaders=[header_value])
+    print('Successful response')
     print(response)
 except Exception as e:
+    print('Error response')
     print(e)
