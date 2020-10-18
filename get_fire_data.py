6f541f42579b45009393904a775de4b2c6afee17
Eugene Van Zyl <eugevanz@gmail.com>
2020-10-18 16:40:01 +0200

    Update get_fire_data.py
    Commented out

--- get_fire_data.py
+++ get_fire_data.py
@@ -1,22 +1,22 @@
-import pyrebase
-import json
+# import pyrebase
+# import json
 
-fireconfig = {
-    "apiKey": "AIzaSyCPexi8M8FFjmqjtoBTp3HJJiHwyk03S5M",
-    "authDomain": "report-manager-e3067.firebaseapp.com",
-    "databaseURL": "https://report-manager-e3067.firebaseio.com",
-    "projectId": "report-manager-e3067",
-    "storageBucket": "report-manager-e3067.appspot.com",
-    "messagingSenderId": "957180191625"
-}
+# fireconfig = {
+#     "apiKey": "AIzaSyCPexi8M8FFjmqjtoBTp3HJJiHwyk03S5M",
+#     "authDomain": "report-manager-e3067.firebaseapp.com",
+#     "databaseURL": "https://report-manager-e3067.firebaseio.com",
+#     "projectId": "report-manager-e3067",
+#     "storageBucket": "report-manager-e3067.appspot.com",
+#     "messagingSenderId": "957180191625"
+# }
 
-firebase = pyrebase.initialize_app(fireconfig)
+# firebase = pyrebase.initialize_app(fireconfig)
 
-# auth = firebase.auth()
+# # auth = firebase.auth()
 
-db = firebase.database()
-response = db.child("incidents").get()
-parseDict = dict(response.val())
+# db = firebase.database()
+# response = db.child("incidents").get()
+# parseDict = dict(response.val())
 
-app_json = json.dumps(parseDict)
-print(app_json)
+# app_json = json.dumps(parseDict)
+# print(app_json)
